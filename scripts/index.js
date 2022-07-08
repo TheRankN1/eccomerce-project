3702bc7c1d7d4f080f64c2bbc617360ead013159
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2022-07-08 18:59:44 +0300

    added accordion and some responsive for mobile

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,21 @@
-
-
 function toggleAccordion(section) {
-     var accordion = document.querySelector(`.accordionContentAcc.${section} > .contentAcc`);
-   if (accordion.style.display === "none") {
-     accordion.style.display = "block";
-   } else {
-     accordion.style.display = "none";
-   }
-   
+  const accordion = document.querySelector(
+    `[data-type=accordion-${section}] > .contentAcc`
+  );
+
+  if (accordion.style.display === "none") {
+    accordion.style.display = "block";
+  } else {
+    accordion.style.display = "none";
   }
+  const icon = document.querySelector(
+    `[data-type=accordion-${section}] > .title-box > .demo-icon`
+  );
+  const isReversed = icon.classList.contains("reverse");
+
+  if (isReversed) {
+    icon.classList.remove("reverse");
+  } else {
+    icon.classList.add("reverse");
+  }
+}
