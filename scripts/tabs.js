ef3f846b2dd92dded209d2f2373da04dea410c4b
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2022-07-13 09:17:39 +0300

    finish desktop detail page without responsive

---
+++ scripts/tabs.js
@@ -0,0 +1,16 @@
+const tabs = document.querySelectorAll(".tabs");
+const tabContents = document.querySelectorAll(".tab");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+    target.classList.add("active");
+  });
+});
